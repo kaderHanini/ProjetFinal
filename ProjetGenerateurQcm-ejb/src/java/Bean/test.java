c357483bf7e7543212ba5c994897e73d8d5ccafe
bachi_000 <bachi_000@Bachir-VAIO.ad.univ-lyon3.fr>
2015-03-05 15:09:20 +0100

    new commit

--- ProjetGenerateurQcm-ejb/src/java/Bean/test.java
+++ ProjetGenerateurQcm-ejb/src/java/Bean/test.java
@@ -9,5 +9,26 @@ package Bean;
  * @author kader
  */
 public class test {
+  
+    private String nom;
+
+    /**
+     * Get the value of nom
+     *
+     * @return the value of nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @param nom new value of nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
     
 }
